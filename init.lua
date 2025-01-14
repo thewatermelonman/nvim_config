180b6b77f4e9808d1f1b5a2392a92310d09be1a6
David Zwick <zwickdavid@yahoo.it>
2025-01-14 20:40:43 +0100

    laptop state 14Jan

--- init.lua
+++ init.lua
@@ -55,7 +55,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{ -- makeit
 		"Zeioth/makeit.nvim",
-		cmd = {"MakeitOpen", "MakeitToggleResults", "MakeitRedo"},
+		cmd = {"MakeitOpen", "MakeitToggleResults", "MakeitRedo", "MakeitStop"},
 		dependencies = { "stevearc/overseer.nvim" },
 		opts = {},
 	},
@@ -222,6 +222,20 @@ require("lazy").setup({
 	},
 	{
 		"lunarvim/colorschemes"
+	},
+	{
+		"zenbones-theme/zenbones.nvim",
+		-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+		-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+		-- In Vim, compat mode is turned on as Lush only works in Neovim.
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
+		-- you can set set configuration options here
+		config = function()
+		    vim.g.zenbones_darken_comments = 45
+		    vim.cmd.colorscheme('zenbones')
+		end
 	},
 	{
 		"RRethy/base16-nvim",
--- lua/config.lua
+++ lua/config.lua
@@ -19,17 +19,23 @@ require('mappings')
 require('lsp.init')
 
 vim.cmd('colorscheme aurora')
-vim.cmd('colorscheme base16-schemer-dark')
+--vim.cmd('colorscheme base16-schemer-dark')
+
+vim.cmd('set termguicolors')
+vim.cmd('set background=dark')
+
+--vim.cmd('colorscheme zenburned')
+
 --[[ vim.opt.shell = "pwsh.exe"
 vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
 vim.cmd [[
 		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 		set shellquote= shellxquote=
-  
+]]  
 
 -- Set a compatible clipboard manager
--[[ vim.g.clipboard = {
+vim.g.clipboard = {
 	copy = {
 		["*"] = "win32yank.exe -i --crlf",
 		["+"] = "win32yank.exe -i --crlf",
@@ -38,6 +44,6 @@ vim.cmd [[
 		["+"] = "win32yank.exe -o --lf",
 		["*"] = "win32yank.exe -o --lf",
 	},
-} ]]
+}
 
 
